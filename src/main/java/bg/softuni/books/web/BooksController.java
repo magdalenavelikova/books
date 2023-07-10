54cbe0901ea7d1f3c86a59b1e5e2e95dd16e7bce
MLVelikova <magibox@yahoo.com>
2023-07-10 14:22:31 +0300

    Post API

--- src/main/java/bg/softuni/books/web/BooksController.java
+++ src/main/java/bg/softuni/books/web/BooksController.java
@@ -4,10 +4,8 @@ import bg.softuni.books.model.dto.BookDTO;
 import bg.softuni.books.service.BookService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,22 +24,51 @@ public class BooksController {
 
   @GetMapping
   public ResponseEntity<List<BookDTO>> getAllBooks() {
-    ResponseEntity<List<BookDTO>> ok = ResponseEntity.
+  return ResponseEntity.
             ok(bookService.getAllBooks());
-    return ok;
+
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<BookDTO> getBookById(@PathVariable("id") Long bookId) {
     Optional<BookDTO> bookOpt = bookService.getBookById(bookId);
-    if (bookOpt.isEmpty()) {
-      return ResponseEntity.
-              notFound().
-              build();
-    } else {
-      return ResponseEntity.
-              ok(bookOpt.get());
-    }
+    return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.
+            notFound().
+            build());
   }
+
+  @PostMapping
+  public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO newBook,
+                                            UriComponentsBuilder uriComponentsBuilder) {
+    long newBookID = bookService.createBook(newBook);
+
+    return
+            ResponseEntity.
+                    created(uriComponentsBuilder.path("/api/books/{id}").
+                            build(newBookID)).
+                    build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<BookDTO> deleteBookById(@PathVariable("id") Long bookId) {
+    bookService.deleteBookById(bookId);
+
+    return ResponseEntity.
+            noContent().
+            build();
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<BookDTO> updateBook(@PathVariable("id") Long id, @RequestBody BookDTO bookDTO) {
+
+    BookDTO updatedBookDTO = this.bookService.persistBook(id, bookDTO);
+
+    if(updatedBookDTO == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(updatedBookDTO);
+  }
+
 
 }
